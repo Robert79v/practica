1477c5de3ce52aaba53dae0fa1a344dc49ccf76f
Roberto <21030097@itcelaya.edu.mx>
2024-11-25 12:53:35 -0600

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
         <Testimonio
-          nombre='Shawn Wang'
+          nombre='Roberto Carlos Andrade Ramírez'
           pais='Singapur'
           imagen='shawn'
           cargo='Ingeniero de Software'
           empresa='Amazon'
           testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'/>
         <Testimonio
-          nombre='Sarah Chima'
+          nombre='Cristian Eduardo Castañeda Pérez'
           pais='Nigeria'
           imagen='sarah'
           cargo='Ingeniera de Software'
